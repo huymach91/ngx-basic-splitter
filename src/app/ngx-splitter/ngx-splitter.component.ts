88d4061402fa7648c87ee77568c25ded3437fc9d
huymach91 <huy.mach.webdev@gmail.com>
2021-11-01 13:12:01 +0700

    2 files modified, 1 added

--- src/app/ngx-splitter/ngx-splitter.component.ts
+++ src/app/ngx-splitter/ngx-splitter.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'ngx-splitter',
+  selector: '[ngx-splitter]',
   templateUrl: './ngx-splitter.component.html',
   styleUrls: ['./ngx-splitter.component.scss'],
 })
-export class NgxSplitterComponent {}
+export class NgxSplitterComponent {
+  @Input('direction') direction: 'vertical' | 'horizontal';
+}
