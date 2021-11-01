d63b0a78179cc6b52552e873e5346ba893aa1404
huymach91 <huy.mach.webdev@gmail.com>
2021-11-01 14:02:50 +0700

    3 files modified

--- src/app/ngx-splitter/ngx-splitter.directive.ts
+++ src/app/ngx-splitter/ngx-splitter.directive.ts
@@ -1,10 +1,31 @@
-import { Directive, Input } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+
+export enum DirectionEnum {
+  vertical = 'vertical',
+  horizontal = 'horizontal',
+}
 
 @Directive({
   selector: '[ngx-splitter]',
 })
-export class NgxSplitterDirective {
-  @Input('direction') direction: 'vertical' | 'horizontal';
+export class NgxSplitterDirective implements AfterViewInit {
+  @Input('direction') direction: DirectionEnum = DirectionEnum.horizontal;
 
-  constructor() {}
+  constructor(private element: ElementRef) {}
+
+  ngAfterViewInit() {
+    console.log('dasdasd');
+    this.setStyle();
+  }
+
+  setStyle() {
+    const element = this.element.nativeElement as HTMLDivElement;
+    element.style.setProperty(
+      'cursor',
+      this.direction === DirectionEnum.horizontal ? 'col-resize' : 'row-resize'
+    );
+    element.style.setProperty('width', '3px');
+    element.style.setProperty('background-color', '#f8f9fa');
+    element.style.setProperty('height', '100%');
+  }
 }
